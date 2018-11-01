29eb0fbebedcf5716dce9ba4dabe2e2ed066c4fc
kttkyk <mmttt202@gmail.com>
2018-11-01 21:06:01 +0900

    Finish 08

---
+++ src/08/os/syscall.h
@@ -0,0 +1,30 @@
+#ifndef _KOZOS_SYSCALL_H_INCLUDED
+#define _KOZOS_SYSCALL_H_INCLUDED
+
+#include "defines.h"
+
+typedef enum {
+    KZ_SYSCALL_TYPE_RUN = 0,
+    KZ_SYSCALL_TYPE_EXIT,
+} kz_syscall_type_t;
+
+
+typedef struct {
+    union {
+        struct {
+            kz_func_t func;
+            char *name;
+            int stacksize;
+            int argc;
+            char **argv;
+            kz_thread_id_t ret;
+        } run;
+
+        struct {
+            int dummy;
+        } exit;
+    } un;
+} kz_syscall_param_t;
+
+
+#endif
