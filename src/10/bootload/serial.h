09cdc59932d5cc94d42ea93a965d50f2b7a7cdd6
kttkyk <mmttt202@gmail.com>
2018-11-17 22:00:41 +0900

    finish 10

---
+++ src/10/bootload/serial.h
@@ -0,0 +1,11 @@
+#ifndef _SERIAL_H_INCLUDED
+#define _SERIAL_H_INCLUDED
+
+
+int serial_init(int index);
+int serial_is_send_enable(int index);
+int serial_send_byte(int index, unsigned char b);
+int serial_is_recv_enable(int index);
+unsigned char serial_recv_byte(int index);
+
+#endif
